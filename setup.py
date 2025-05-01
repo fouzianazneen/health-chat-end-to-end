541f973c1619d8f2e95e19c83093a1ea0eeef4e6
fouzia nazneen <fouzianazneen236@gmail.com>
2025-05-01 16:38:00 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical_chatbot',      # ✅ no spaces or special characters
+    version='0.1',               # ✅ clean version format
+    author='fouzia',
+    author_email='fouzianazneen1216@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
