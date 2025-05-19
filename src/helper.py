d4682343b6c0dd1139c04b90c56b5049be15e166
fouzia nazneen <fouzianazneen236@gmail.com>
2025-05-19 22:09:24 +0530

    all chnge done

--- src/helper.py
+++ src/helper.py
@@ -1,34 +1,4 @@
 
-<<<<<<< HEAD
-=======
-# from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_huggingface import HuggingFaceEmbeddings  # ✅ updated import
-
-# def load_pdf(data):
-#     loader = DirectoryLoader(
-#         data,
-#         glob="*.pdf",
-#         loader_cls=PyPDFLoader
-#     )
-#     documents = loader.load()
-#     return documents
-
-# # Create text chunks
-# def text_split(extracted_data):
-#     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=20)
-#     text_chunks = text_splitter.split_documents(extracted_data)
-#     return text_chunks
-
-# # Download embedding model
-# def download_hugging_face_embeddings():
-#     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-#     return embeddings
-
-
-
-
->>>>>>> 41c3c380f9fbe7c2a97004fa79baf3eccb4afe49
 
 
 
